f75db619190a3eabd2b4dba5d0f30a2ca1dfddb3
FreezerNick <nl@f2p-entertainment.de>
2019-09-29 14:04:14 +0200

    Progress

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 
 namespace gitkab
 {
@@ -42,8 +43,7 @@ namespace gitkab
                 else if (Console.ReadKey().Key == ConsoleKey.N)
                 {
                     Console.WriteLine("Done!");
-                    // save & close
-                    // get structure
+                    GetNamespaces(Key);
                     return 1;
                 }
                 else
@@ -54,11 +54,25 @@ namespace gitkab
                     return 1;
                 }
             }
+            else if (Array.IndexOf(args, "init") > -1)
+            {
+                return 1;
+            }
             else
             {
                 Console.WriteLine("What should I do?");
                 return 1;
             }
+        }
+
+        static void GetNamespaces(string PrivateKey)
+        {
+            WebRequest request = WebRequest.Create("https://gitlab.com/api/v4/namespaces?access_token=" + PrivateKey);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            response.Close();
+            StreamReader responseReader = new StreamReader(dataStream);
+            Console.WriteLine(responseReader.ReadToEnd());
         }
     }
 }
