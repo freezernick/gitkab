e3edeea165696df8ecd493fb3d9136e2a34bffb9
FreezerNick <nl@f2p-entertainment.de>
2019-10-05 15:11:52 +0200

    Basic parsing; Closes #3

---
+++ gitkab.py
@@ -0,0 +1,33 @@
+import argparse, requests, os
+
+# Used for loading the config
+scriptDirectory = os.path.dirname(__file__)
+
+# argparse
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+config_parser = subparsers.add_parser('config') # gitkab config
+config_subparser = config_parser.add_subparsers()
+config_parser.add_argument('--key')
+
+config_user = config_subparser.add_parser('user') # gitkab config user ...
+config_user_group = config_user.add_mutually_exclusive_group(required=True)
+config_user_group.add_argument('--list', action='store_true') # gitkab config user --list
+config_user_group.add_argument('--add', action='store_true') # gitkab config user --add
+config_user_group.add_argument('--remove', action='store_true') # gitkab config user --remove
+
+def listUsers(args):
+    print("list")
+
+def addUser(args):
+    print("add")
+
+def removeUser(args):
+    print("remove")
+
+args = parser.parse_args()
+
+if(args.key):
+    # save / overwrite api key
+    print("Updated key")
